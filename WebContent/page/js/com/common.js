d45d033cb4fc65be2571272b13937a7d79c7f925
xxxx@xxx.xx.xx <xxxx@xxx.xx.xx>
2016-06-20 11:19:09 +0900

    commit

--- WebContent/page/js/com/common.js
+++ WebContent/page/js/com/common.js
@@ -2,11 +2,16 @@
  * CSSスタイル変更
  */
 function changeStyle(style){
+	// フェードアウト後にCSS変更
 	$("body").fadeOut("normal", function(){
 		$("#mystyle").attr("href", "../fw/bootstrap/css/bootswatch-theme-"+ style +".css");
 	});
+
+	// N秒後にフェードイン
 	setTimeout(function(){
 		$("body").fadeIn("normal");
-	},2000);
+	},1500);
 
+	// TODO ローカルストレージに保存
+	localStorage.setItem("mystyle", style);
 }
