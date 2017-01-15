e7acde130d1477a32bd248aa72ba6ef199577ffe
7days <7days@7days-PC>
2017-01-15 23:19:19 +0900

    commit

--- src/main/resources/static/js/com/common.js
+++ src/main/resources/static/js/com/common.js
@@ -5,11 +5,12 @@
  */
 function changeTheme(theme) {
 
-	var cssUrl = "/fw/bootstrap/css/bootswatch-theme-" + theme + ".min.css";
+	var cssBaseUrl = $("form").attr("action") + "fw/bootstrap/css/";
+	var cssName = "bootswatch-theme-" + theme + ".min.css";
 
 	// フェードアウト後にCSS変更
 	$("body").fadeOut("normal", function() {
-		$("#myTheme").attr("href", cssUrl);
+		$("#myTheme").attr("href", cssBaseUrl + cssName);
 	});
 
 	// N秒後にフェードイン
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -28,7 +28,7 @@ $(function() {
 		// ローカルストレージからテーマを取得
 		var theme = localStorage.getItem("theme");
 		if (theme != null && theme != "") {
-			if (theme != $("#theme").val()) {
+			if (theme != $("#myTheme").val()) {
 				// テーマ変更
 				changeTheme(theme);
 			}
