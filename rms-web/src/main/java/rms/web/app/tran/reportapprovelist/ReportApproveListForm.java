451bc5b56fca675e731241a6e9cba81470092872
7days <7days@7DAYS-PC>
2017-11-26 20:17:52 +0900

    １ページ表示件数を変更

--- rms-web/src/main/java/rms/web/app/tran/reportapprovelist/ReportApproveListForm.java
+++ rms-web/src/main/java/rms/web/app/tran/reportapprovelist/ReportApproveListForm.java
@@ -27,7 +27,7 @@ public class ReportApproveListForm extends rms.common.abstracts.AbstractForm {
     @Valid
     private ReportApproveListFormCondition condition = new ReportApproveListFormCondition();
     /** ページ情報 */
-    private PageInfo pageInfo = new PageInfo(Integer.MAX_VALUE);
+    private PageInfo pageInfo = new PageInfo(PageInfo.LIMIT_100);
     /** 検索結果リスト */
     private List<ReportApproveListEntityResult> resultList;
     /** 検索結果チェックボックス選択リスト */
