3625c2d9e5abc4524fa5dafddd8ea40acbfadf0c
7days <7days@7DAYS-PC>
2017-12-07 23:09:34 +0900

    IEで構文エラー扱いにならないように修正

--- rms-web/src/main/resources/static/js/com/rms.js
+++ rms-web/src/main/resources/static/js/com/rms.js
@@ -6,40 +6,35 @@ var Rms = function() {
 };
 
 /**
- * 関数定義
+ * CSSテーマ変更
+ * 
+ * @param theme
  */
-Rms.prototype = {
+Rms.prototype.changeTheme = function(theme) {
 
-	/**
-	 * CSSテーマ変更
-	 * 
-	 * @param theme
-	 */
-	changeTheme(theme) {
-		if (theme === $("#theme").val()) {
-			// 現在と同じテーマの場合は何もしない
-			return;
-		}
-
-		var cssBaseUrl = $("form").attr("action") + "fw/bootstrap/css/";
-		var cssName = "bootswatch-theme-" + theme + ".min.css";
-
-		// フェードアウト後にCSS変更
-		$("body").fadeOut("normal", function() {
-			$("#myTheme").attr("href", cssBaseUrl + cssName);
-		});
-
-		// N秒後にフェードイン
-		setTimeout(function() {
-			$("body").fadeIn("normal");
-		}, 1500);
-
-		// ローカルストレージにテーマを保存
-		localStorage.setItem("theme", theme);
-		// hiddenの値を更新
-		$("#theme").val(theme);
+	if (theme === $("#theme").val()) {
+		// 現在と同じテーマの場合は何もしない
+		return;
 	}
-};
+
+	var cssBaseUrl = $("form").attr("action") + "fw/bootstrap/css/";
+	var cssName = "bootswatch-theme-" + theme + ".min.css";
+
+	// フェードアウト後にCSS変更
+	$("body").fadeOut("normal", function() {
+		$("#myTheme").attr("href", cssBaseUrl + cssName);
+	});
+
+	// N秒後にフェードイン
+	setTimeout(function() {
+		$("body").fadeIn("normal");
+	}, 1500);
+
+	// ローカルストレージにテーマを保存
+	localStorage.setItem("theme", theme);
+	// hiddenの値を更新
+	$("#theme").val(theme);
+}
 
 /*
  * グローバル変数
