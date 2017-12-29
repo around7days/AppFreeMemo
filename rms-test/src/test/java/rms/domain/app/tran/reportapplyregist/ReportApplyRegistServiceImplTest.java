2c2f293917bda64e2ae2f43c8daae4ab76b7db94
7days <7days@7DAYS-PC>
2017-12-29 15:38:24 +0900

    テストコードの修正

---
+++ rms-test/src/test/java/rms/domain/app/tran/reportapplyregist/ReportApplyRegistServiceImplTest.java
@@ -0,0 +1,118 @@
+package rms.domain.app.tran.reportapplyregist;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import rms.SpringBatchApplication;
+import rms.SpringWebApplication;
+import rms.common.consts.MCodeConst;
+import rms.common.consts.MessageEnum;
+import rms.common.dao.TReportDao;
+import rms.common.dao.VTReportDao;
+import rms.common.entity.TReport;
+import rms.common.exception.BusinessException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringWebApplication.class, properties = "spring.profiles.active=test")
+public class ReportApplyRegistServiceImplTest {
+
+    /* テスト対象 */
+    @Autowired
+    ReportApplyRegistServiceImpl serviceImpl;
+
+    /* Mock対象Dao */
+    @MockBean
+    TReportDao tReportDao;
+    @MockBean
+    VTReportDao vTReportDao;
+
+    @BeforeClass
+    public static void beforeAll() {
+        SpringApplication application = new SpringApplication(SpringBatchApplication.class);
+        application.setWebEnvironment(false); // 内臓tomcatの起動を抑制
+    }
+
+    @Test
+    public void test_validateUniquReport_月報の重複チェック_正常() throws Exception {
+
+        // Mock定義 ---------------------------------------------------
+        doReturn(null).when(tReportDao).selectById(anyObject(), anyObject());
+        // -----------------------------------------------------------
+
+        // テスト実行
+        serviceImpl.validateUniquReport("dummy", 999912);
+
+        // テスト結果確認（到達確認）
+        assertTrue("正常終了", true);
+    }
+
+    @Test
+    public void test_validateUniquReport_月報の重複チェック_重複エラー() throws Exception {
+
+        // Mock定義 ---------------------------------------------------
+        TReport entity = new TReport();
+        entity.setStatus(MCodeConst.A001_Y01);
+        doReturn(entity).when(tReportDao).selectById(anyObject(), anyObject());
+        // -----------------------------------------------------------
+
+        try {
+            // テスト実行
+            serviceImpl.validateUniquReport("dummy", 999912);
+        } catch (BusinessException e) {
+            // テスト結果確認
+            assertThat(e.getErrorCode(), is(MessageEnum.error003.name()));
+            return;
+        }
+
+        fail("失敗");
+    }
+
+    @Test
+    public void test_validateFutureYm_月報の未来日付チェック_正常() throws Exception {
+
+        // テスト実行
+        serviceImpl.validateFutureYm(200001);
+
+        // テスト結果確認（到達確認）
+        assertTrue("正常終了", true);
+    }
+
+    @Test
+    public void test_insertReport_月報申請処理() throws Exception {
+
+        // Mock定義 ---------------------------------------------------
+        doReturn(1).when(tReportDao).insert(anyObject());
+        // -----------------------------------------------------------
+
+        // テストデータ
+        ReportApplyRegistDto dto = new ReportApplyRegistDto();
+        dto.setApplyUserId("ユーザID");
+        dto.setTargetYm(999912);
+
+        // テスト実行
+        serviceImpl.insertReport(dto);
+
+        // テスト結果確認（到達確認）
+        assertTrue("正常終了", true);
+
+        // TODO メソッドを呼び出した引数のチェックを行う方法が分からない・・・
+        // // テスト結果確認（呼び出し確認）
+        // TReport entity = new TReport();
+        // entity.setApplyUserId("ユーザID");
+        // entity.setTargetYm(999912);
+        // entity.setFilePath("");
+        // entity.setStatus(MCodeConst.A001_AAA);
+        // verify(tReportDao).insert(entity);
+    }
+}
--- rms-test/src/test/java/rms/domain/app/tran/reportapplyregist/ReportApplyRegistServiceTest.java
+++ rms-test/src/test/java/rms/domain/app/tran/reportapplyregist/ReportApplyRegistServiceTest.java
@@ -5,215 +5,155 @@ import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.multipart.MultipartFile;
 
+import rms.SpringBatchApplication;
 import rms.SpringWebApplication;
-import rms.common.consts.MCodeConst;
-import rms.common.consts.MessageEnum;
-import rms.common.dao.TReportDao;
+import rms.common.dao.VMUserDao;
 import rms.common.dao.VTReportDao;
-import rms.common.entity.TReport;
+import rms.common.entity.VMUser;
 import rms.common.entity.VTReport;
-import rms.common.exception.BusinessException;
-import rms.testutil.mock.RmsMockFileType;
-import rms.testutil.mock.RmsMockUtils;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringWebApplication.class)
+@SpringBootTest(classes = SpringWebApplication.class, properties = "spring.profiles.active=test")
 public class ReportApplyRegistServiceTest {
 
+    /* テスト対象 */
     @Autowired
     ReportApplyRegistService service;
-    @Autowired
+
+    /* Mock対象Service */
+    @SpyBean
     ReportApplyRegistServiceImpl serviceImpl;
-    @Autowired
+
+    /* Mock対象Dao */
+    @MockBean
+    VMUserDao vMUserDao;
+    @MockBean
     VTReportDao vTReportDao;
 
-    @SpyBean
-    TReportDao tReportDao;
+    @BeforeClass
+    public static void beforeAll() {
+        SpringApplication application = new SpringApplication(SpringBatchApplication.class);
+        application.setWebEnvironment(false); // 内臓tomcatの起動を抑制
+    }
 
     @Test
     public void test_initDisplayApply_月報申請_初期表示処理() {
 
+        // Mock定義 ---------------------------------------------------
+        VMUser mockEntity = new VMUser();
+        mockEntity.setUserId("ユーザID");
+        mockEntity.setUserNm("ユーザ名");
+        mockEntity.setApproveUserId1("承認者ID1");
+        mockEntity.setApproveUserId2("承認者ID2");
+        mockEntity.setApproveUserId3("承認者ID3");
+        mockEntity.setApproveUserId4("承認者ID4");
+        mockEntity.setApproveUserNm1("承認者名1");
+        mockEntity.setApproveUserNm2("承認者名2");
+        mockEntity.setApproveUserNm3("承認者名3");
+        mockEntity.setApproveUserNm4("承認者名4");
+        doReturn(mockEntity).when(vMUserDao).selectById(anyObject());
+        // -----------------------------------------------------------
+
+        // テスト実行
         ReportApplyRegistDto dto = service.initDisplayApply("user01");
 
-        assertThat(dto.getApplyUserId(), is("user01"));
-        assertThat(dto.getApplyUserNm(), is("申請者０１"));
-        // assertThat(dto.getTargetYm(), is(nullValue())); TODO システム日付を指定してテストする必要あり
+        // テスト結果確認
+        assertThat(dto.getApplyUserId(), is("ユーザID"));
+        assertThat(dto.getApplyUserNm(), is("ユーザ名"));
+        assertThat(dto.getApproveUserId1(), is("承認者ID1"));
+        assertThat(dto.getApproveUserId2(), is("承認者ID2"));
+        assertThat(dto.getApproveUserId3(), is("承認者ID3"));
+        assertThat(dto.getApproveUserId4(), is("承認者ID4"));
+        assertThat(dto.getApproveUserNm1(), is("承認者名1"));
+        assertThat(dto.getApproveUserNm2(), is("承認者名2"));
+        assertThat(dto.getApproveUserNm3(), is("承認者名3"));
+        assertThat(dto.getApproveUserNm4(), is("承認者名4"));
+        assertThat(dto.getTargetYm(), is(201706));
         assertThat(dto.getFile(), is(nullValue()));
-        assertThat(dto.getApproveUserId1(), is("user06"));
-        assertThat(dto.getApproveUserId2(), is("user07"));
-        assertThat(dto.getApproveUserId3(), is("user08"));
-        assertThat(dto.getApproveUserNm1(), is("承認者０１"));
-        assertThat(dto.getApproveUserNm2(), is("承認者０２"));
-        assertThat(dto.getApproveUserNm3(), is("承認者０３"));
     }
 
     @Test
     public void test_initDisplayReApply_月報再申請_初期表示処理() {
 
-        ReportApplyRegistDto dto = service.initDisplayReApply("user01", 201609);
+        // Mock定義 ---------------------------------------------------
+        VTReport mockEntity = new VTReport();
+        mockEntity.setApplyUserId("ユーザID");
+        mockEntity.setApplyUserNm("ユーザ名");
+        mockEntity.setApproveUserId1("承認者ID1");
+        mockEntity.setApproveUserId2("承認者ID2");
+        mockEntity.setApproveUserId3("承認者ID3");
+        mockEntity.setApproveUserId4("承認者ID4");
+        mockEntity.setApproveUserNm1("承認者名1");
+        mockEntity.setApproveUserNm2("承認者名2");
+        mockEntity.setApproveUserNm3("承認者名3");
+        mockEntity.setApproveUserNm4("承認者名4");
+        doReturn(mockEntity).when(vTReportDao).selectById(anyObject(), anyObject());
+        // -----------------------------------------------------------
 
-        assertThat(dto.getApplyUserId(), is("user01"));
-        assertThat(dto.getApplyUserNm(), is("申請者０１"));
-        assertThat(dto.getTargetYm(), is(201609));
-        assertThat(dto.getFile(), is(nullValue()));
-        assertThat(dto.getApproveUserId1(), is("user06"));
-        assertThat(dto.getApproveUserId2(), is("user07"));
-        assertThat(dto.getApproveUserId3(), is("user08"));
-        assertThat(dto.getApproveUserNm1(), is("承認者０１"));
-        assertThat(dto.getApproveUserNm2(), is("承認者０２"));
-        assertThat(dto.getApproveUserNm3(), is("承認者０３"));
+        // テスト実行
+        ReportApplyRegistDto dto = service.initDisplayReApply("dummy", 999912);
+
+        // テスト結果確認
+        assertThat(dto.getApplyUserId(), is("ユーザID"));
+        assertThat(dto.getApplyUserNm(), is("ユーザ名"));
+        assertThat(dto.getApproveUserId1(), is("承認者ID1"));
+        assertThat(dto.getApproveUserId2(), is("承認者ID2"));
+        assertThat(dto.getApproveUserId3(), is("承認者ID3"));
+        assertThat(dto.getApproveUserId4(), is("承認者ID4"));
+        assertThat(dto.getApproveUserNm1(), is("承認者名1"));
+        assertThat(dto.getApproveUserNm2(), is("承認者名2"));
+        assertThat(dto.getApproveUserNm3(), is("承認者名3"));
+        assertThat(dto.getApproveUserNm4(), is("承認者名4"));
     }
 
-    @Test
-    public void test_apply_月報申請_重複チェックエラー処理() throws Exception {
-
-        String applyUserId = "user01";
-        Integer targetYm = 201608;
-
-        ReportApplyRegistDto dto = new ReportApplyRegistDto();
-        dto.setApplyUserId(applyUserId);
-        dto.setTargetYm(targetYm);
-        try {
-            service.apply(dto);
-        } catch (BusinessException e) {
-            assertThat(e.getErrorMessage(), is("対象年月の月報は既に申請されています"));
-            return;
-        }
-
-        fail("エラーチェック失敗");
-    }
-
-    @Test
-    public void test_apply_月報申請_未来日付チェックエラー_来月の月報を提出() throws Exception {
-
-        LocalDate targetDate = LocalDate.now().plusMonths(1);
-
-        String applyUserId = "user01";
-        Integer targetYm = Integer.valueOf(targetDate.format(DateTimeFormatter.ofPattern("yyyyMM")));
-
-        ReportApplyRegistDto dto = new ReportApplyRegistDto();
-        dto.setApplyUserId(applyUserId);
-        dto.setTargetYm(targetYm);
-        try {
-            service.apply(dto);
-        } catch (BusinessException e) {
-            assertThat(e.getErrorMessage(),
-                       is(allOf(org.hamcrest.CoreMatchers.startsWith("対象年月の月報は"),
-                                org.hamcrest.CoreMatchers.endsWith("以降から申請可能です"))));
-            return;
-        }
-
-        fail("エラーチェック失敗");
-    }
-
-    @WithUserDetails(value = "user01", userDetailsServiceBeanName = "userDetailsServiceImpl")
     @Test
     public void test_apply_月報申請処理() throws Exception {
 
-        String applyUserId = "user01";
-        Integer targetYm = 201606;
-
-        // 月報申請処理
-        {
-            ReportApplyRegistDto dto = new ReportApplyRegistDto();
-            dto.setApplyUserId(applyUserId);
-            dto.setTargetYm(targetYm);
-            dto.setApproveUserId1("user06");
-            dto.setApproveUserId2("user07");
-            dto.setApproveUserId3("user08");
-            MultipartFile file = RmsMockUtils.getDummyMultipartFile(RmsMockFileType.XLSX);
-            dto.setFile(file);
-
-            service.apply(dto);
-        }
-
-        // DB登録内容の確認
-        {
-            VTReport vTReport = vTReportDao.selectById(applyUserId, targetYm);
-            assertThat(vTReport.getApplyUserId(), is(applyUserId));
-            assertThat(vTReport.getTargetYm(), is(targetYm));
-            assertThat(vTReport.getApplyDate().toLocalDate(), is(LocalDate.now()));
-            assertThat(vTReport.getApproveUserId1(), is("user06"));
-            assertThat(vTReport.getApproveUserId2(), is("user07"));
-            assertThat(vTReport.getApproveUserId3(), is("user08"));
-            assertThat(vTReport.getApproveDate1(), is(nullValue()));
-            assertThat(vTReport.getApproveDate2(), is(nullValue()));
-            assertThat(vTReport.getApproveDate3(), is(nullValue()));
-            assertThat(vTReport.getStatus(), is("Y01"));
-        }
-
-        // TODO ファイル登録内容の確認は未実装
-        // fail("ファイル確認は未実装");
-    }
-
-    @WithUserDetails(value = "user01", userDetailsServiceBeanName = "userDetailsServiceImpl")
-    @Test
-    public void test_reApply_月報再申請処理() throws Exception {
-        String applyUserId = "user01";
-        Integer targetYm = 201609;
-
-        // 月報再申請処理
-        {
-            ReportApplyRegistDto dto = new ReportApplyRegistDto();
-            dto.setApplyUserId(applyUserId);
-            dto.setTargetYm(targetYm);
-            dto.setApproveUserId1("user06");
-            dto.setApproveUserId2("user07");
-            dto.setApproveUserId3("user08");
-            MultipartFile file = RmsMockUtils.getDummyMultipartFile(RmsMockFileType.XLSX);
-            dto.setFile(file);
-            dto.setVersion(0);
-            service.reApply(dto);
-        }
-
-        // DB登録内容の確認
-        {
-            VTReport vTReport = vTReportDao.selectById(applyUserId, targetYm);
-            assertThat(vTReport.getApplyUserId(), is(applyUserId));
-            assertThat(vTReport.getTargetYm(), is(targetYm));
-            assertThat(vTReport.getApplyDate().toLocalDate(), is(LocalDate.now()));
-            assertThat(vTReport.getApproveUserId1(), is("user06"));
-            assertThat(vTReport.getApproveUserId2(), is("user07"));
-            assertThat(vTReport.getApproveUserId3(), is("user08"));
-            assertThat(vTReport.getApproveDate1(), is(nullValue()));
-            assertThat(vTReport.getApproveDate2(), is(nullValue()));
-            assertThat(vTReport.getApproveDate3(), is(nullValue()));
-            assertThat(vTReport.getStatus(), is("Y01"));
-        }
-
-        // TODO ファイル登録内容の確認は未実装
-        // fail("ファイル確認は未実装");
-    }
-
-    @WithUserDetails(value = "user01", userDetailsServiceBeanName = "userDetailsServiceImpl")
-    @Test
-    public void test_validateUniquReport_月報の重複チェック() throws Exception {
-
         // Mock定義 ---------------------------------------------------
-        TReport entity = new TReport();
-        entity.setStatus(MCodeConst.A001_Y01);
-        doReturn(entity).when(tReportDao).selectById(anyString(), anyInt());
+        doNothing().when(serviceImpl).validateUniquReport(anyObject(), anyObject());
+        doNothing().when(serviceImpl).validateFutureYm(anyObject());
+        doNothing().when(serviceImpl).deleteReport(anyObject());
+        doNothing().when(serviceImpl).deleteReportApproveFlow(anyObject());
+        doNothing().when(serviceImpl).insertReport(anyObject());
+        doNothing().when(serviceImpl).insertReportApproveFlow(anyObject());
+        doNothing().when(serviceImpl).updateReportStatus(anyObject());
+        doNothing().when(serviceImpl).saveReportFile(anyObject());
         // -----------------------------------------------------------
 
-        try {
-            serviceImpl.validateUniquReport("dummy", 999999);
-        } catch (BusinessException e) {
-            assertThat(e.getErrorCode(), is(MessageEnum.error003.name()));
-            return;
-        }
+        // テスト実行
+        service.apply(new ReportApplyRegistDto());
 
-        fail("失敗");
+        // テスト結果確認（到達確認）
+        assertTrue("正常終了", true);
     }
+
+    @Test
+    public void test_reApply_月報再申請処理() throws Exception {
+
+        // Mock定義 ---------------------------------------------------
+        doNothing().when(serviceImpl).deleteReport(anyObject());
+        doNothing().when(serviceImpl).deleteReportApproveFlow(anyObject());
+        doNothing().when(serviceImpl).insertReport(anyObject());
+        doNothing().when(serviceImpl).insertReportApproveFlow(anyObject());
+        doNothing().when(serviceImpl).updateReportStatus(anyObject());
+        doNothing().when(serviceImpl).saveReportFile(anyObject());
+        // -----------------------------------------------------------
+
+        // テスト実行
+        service.reApply(new ReportApplyRegistDto());
+
+        // テスト結果確認（到達確認）
+        assertTrue("正常終了", true);
+    }
+
 }
